e9190a9fa91a4e2cd1957e51e42e8369d964a757
latoyale <llegemah@gmail.com>
2018-01-26 06:41:06 -0500

    pulling in data into the page

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface ItemsResponse {
+  results: string[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent implements OnInit{
   title = 'app';
-   results: string[];
+ results: string[];
+
 
   constructor(private http: HttpClient){ }
 
   ngOnInit(): void {
-    this.http.get('https://api.github.com/users/seeschweiler').subscribe(data => {
+    this.http.get<ItemsResponse>('https://api.github.com/users/seeschweiler').subscribe(data => {
       console.log(data);
-      this.results = data['results'];
+      this.results = data;
     });
   }
 
