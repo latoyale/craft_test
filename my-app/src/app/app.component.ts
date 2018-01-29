eb86a54eb9ce7551e4290920144cd08f59ed7910
latoyale <llegemah@gmail.com>
2018-01-28 19:14:13 -0500

    reverting

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface ItemsResponse {
+  results: any[];
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+
   styleUrls: ['./app.component.css']
 })
+
 
 export class AppComponent implements OnInit{
   title = 'app';
@@ -15,9 +20,9 @@ export class AppComponent implements OnInit{
   constructor(private http: HttpClient){ }
 
   ngOnInit(): void {
-    this.http.get('https://api.github.com/users/seeschweiler').subscribe(data => {
+    this.http.get<ItemsResponse>('https://api.github.com/users/seeschweiler').subscribe(data => {
       console.log(data);
-      this.results = data['results'];
+      this.results = data;
     });
   }
 
