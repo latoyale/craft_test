6f5c97521151c55a8b291b8bf47bbfbd0c67e4f3
latoyale <llegemah@gmail.com>
2018-01-28 19:00:56 -0500

    updates

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface ItemsResponse {
-  results: any[];
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 
 export class AppComponent implements OnInit{
   title = 'app';
@@ -22,7 +17,7 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.http.get('https://api.github.com/users/seeschweiler').subscribe(data => {
       console.log(data);
-      this.results = data;
+      this.results = data['results'];
     });
   }
 
